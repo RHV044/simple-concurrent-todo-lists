71519d32ee05e14b65d055645c3d591e54bed27e
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-06-28 00:26:33 -0300

    General backend api call method

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -8,17 +8,11 @@ $(document).ready(function () {
 
 function updateLists() {
     console.log("Updating lists");
-    askForNode()
-        .then((response) => $.ajax({
-            url: `http://localhost:${response.port}/lists`,
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }))
+    doBackendApiCall("GET", "lists")
         .then((response) => response.forEach((todoList) => {
             if ($(`#todo-list-hash-${todoList.id}`).length == 0) {
                 addListView(todoList);
-            } else if ($(`#todo-list-hash-${todoList.id}`).val() != todoList.hashVersion) { 
+            } else if ($(`#todo-list-hash-${todoList.id}`).val() != todoList.hashVersion) {
                 // TODO: test it when hash is implemented (it's working now because "null" != null no it's updating allways :))
                 updateListView(todoList);
             }
@@ -31,19 +25,13 @@ function addTodoList(id) {
         showError("Error al crear la lista", "Por favor complete el titulo de la lista")
         return;
     }
-    askForNode()
-        .then((response) => $.ajax({
-            url: `http://localhost:${response.port}/lists`,
-            type: "POST",
-            data: JSON.stringify({
-                list: {
-                    title: title,
-                    creator: USER_NAME
-                }
-            }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }))
+    doBackendApiCall("POST", "lists",
+        {
+            list: {
+                title: title,
+                creator: USER_NAME
+            }
+        })
         .then((response) => addListView(response))
         .fail(() => showError("Error al crear la lista", "Hubo un error al intentar la creación de la lista, intentelo nuevamente"));
 }
@@ -54,19 +42,13 @@ function addTodoListTask(id) {
         showError("Error al crear la tarea", "Por favor complete el texto")
         return;
     }
-    askForNode()
-        .then((response) => $.ajax({
-            url: `http://localhost:${response.port}/lists/${id}/items`,
-            type: "POST",
-            data: JSON.stringify({
-                item: {
-                    text: task,
-                    done: false
-                }
-            }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }))
+    doBackendApiCall("POST", `lists/${id}/items`,
+        {
+            item: {
+                text: task,
+                done: false
+            }
+        })
         .then((response) => updateListView(response.list))
         .fail(() => showError("Error al crear la tarea", "Hubo un error al intentar la creación de la tarea, intentelo nuevamente"));
 }
@@ -78,29 +60,17 @@ function editTask(listId, taskIndex, task) {
         showError("Error al modificar la tarea", "Por favor complete el texto")
         return;
     }
-    askForNode()
-        .then((response) => $.ajax({
-            url: `http://localhost:${response.port}/lists/${listId}/items/${taskIndex}`,
-            type: "PUT",
-            data: JSON.stringify({
-                text: newTask
-            }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }))
+    doBackendApiCall("PUT", `lists/${listId}/items/${taskIndex}`,
+        {
+            text: newTask
+        })
         .then((response) => updateListView(response.list))
         .fail(() => showError("Error al modificar la tarea", "Hubo un error al intentar la creación de la tarea, intentelo nuevamente"));
 
 }
 
 function toggleTaskChecked(listId, taskIndex, actualStatus) {
-    askForNode()
-        .then((response) => $.ajax({
-            url: `http://localhost:${response.port}/lists/${listId}/items/${taskIndex}/done?status=${!actualStatus}`,
-            type: "PATCH",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }))
+    doBackendApiCall("PATCH", `lists/${listId}/items/${taskIndex}/done?status=${!actualStatus}`)
         .then((response) => updateListView(response.list))
         .fail(() => showError("Error al modificar la tarea", "Hubo un error al intentar la creación de la tarea, intentelo nuevamente"));
 }
@@ -115,7 +85,7 @@ function addListView(todoList) {
 
 function updateListView(todoList) {
     $(`#todo-list-hash-${todoList.id}`).val(todoList.hashVersion);
-    $(`#todo-list-container-${todoList.id} .todo-list`).html(todoList.list.reduce((acc, task, index) => acc + TODO_LIST_TASK_HTML
+    $(`#todo-list-container-${todoList.id} .todo-list`).html(todoList.list.filter((task) => task != null).reduce((acc, task, index) => acc + TODO_LIST_TASK_HTML
         .replaceAll('{todo_list_task_completed}', task.done.toString().toLowerCase() == "true" ? 'completed' : '')
         .replaceAll('{todo_list_task_checked}', task.done.toString().toLowerCase() == "true" ? 'checked' : '')
         .replaceAll('{todo_list_task_is_checked}', task.done.toString().toLowerCase() == "true" ? 'true' : 'false')
@@ -124,7 +94,19 @@ function updateListView(todoList) {
         .replaceAll('{todo_list_task}', task.text), ""));
 }
 
-function askForNode(action) {
+function doBackendApiCall(type, endpoint, body = null) {
+    console.log(`${type} ${endpoint}`, body)
+    return askForNode()
+        .then((response) => $.ajax({
+            url: `http://localhost:${response.port}/${endpoint}`,
+            type: type,
+            data: body != null ? JSON.stringify(body) : null,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }))
+}
+
+function askForNode() {
     return $.get('http://localhost:9000/node/any');
 }
 
